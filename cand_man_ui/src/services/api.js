d2bf7704a59297faf6b5a219bd22bac96667ce0c
BrunoSilva <brunosilva16@ua.pt>
2024-07-19 16:27:21 +0100

    final updates on the platform

--- cand_man_ui/src/services/api.js
+++ cand_man_ui/src/services/api.js
@@ -1,15 +1,49 @@
 import axios from 'axios';
+import Cookies from 'js-cookie';
+
+const baseURL = process.env.REACT_APP_API_URL;
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api', 
+  baseURL: baseURL,
 });
 
-api.interceptors.request.use(config => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+const fetchToken = async () => {
+  const username = process.env.REACT_APP_API_USERNAME;
+  const password = process.env.REACT_APP_API_PASSWORD;
+
+  try {
+    const response = await axios.post(`${baseURL}/token/`, {
+      username,
+      password,
+    });
+    const { access } = response.data;
+    // Set cookie to expire in 1 hour
+    Cookies.set('token', access, { expires: 1/24 }); 
+    return access;
+  } catch (error) {
+    console.error('Error fetching token', error);
+    throw error;
   }
-  return config;
-});
+};
+
+api.interceptors.request.use(
+  async (config) => {
+    let token = Cookies.get('token');
+    if (!token) {
+      token = await fetchToken();
+    } 
+    // else {
+    //   console.log('Token found in cookies:', token);
+    // }
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 
 export default api;
